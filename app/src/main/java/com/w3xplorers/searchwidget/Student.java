68d98b4ddca8db34afa8bd80fb7852b52507f015
Avijit Nandy <avijitcse12@gmail.com>
2017-08-24 12:03:30 +0600

    Initial commit

---
+++ app/src/main/java/com/w3xplorers/searchwidget/Student.java
@@ -0,0 +1,23 @@
+package com.w3xplorers.searchwidget;
+
+/**
+ * Created by Avijit on 7/3/2017.
+ */
+
+public class Student {
+    // Labels table name
+    public static final String TABLE = "Student";
+
+    // Labels Table Columns names
+    public static final String KEY_ROWID = "_id";
+    public static final String KEY_ID = "id";
+    public static final String KEY_name = "name";
+    public static final String KEY_email = "email";
+    public static final String KEY_age = "age";
+
+    // property help us to keep data
+    public int student_ID;
+    public String name;
+    public String email;
+    public int age;
+}
